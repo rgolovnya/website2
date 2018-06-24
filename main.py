58d4af9e703e6003779ebc3af3c2f039fa91d0ef
Stefano Galeano <stefano.galeano@digitary.net>
2018-06-24 16:19:43 +0100

    refactoring

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask, url_for
 from flask import request
 from flask import render_template
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
-@app.route('/index/')
+@application.route('/')
+@application.route('/index/')
 def index():
     return render_template('index.html')
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0')
